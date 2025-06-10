b83c35a67b9d452ffdfa366fddd29043cb7a44c0
cardosofiles <cardosofiles@outlook.com>
2025-06-09 23:32:34 -0300

    chore: vacancy management projects, configuring spring boot

---
+++ src/main/java/br/com/cardosofiles/vacancy_management_server/VacancyManagementServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cardosofiles.vacancy_management_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cardosofiles/vacancy_management_server/VacancyManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardosofiles.vacancy_management_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

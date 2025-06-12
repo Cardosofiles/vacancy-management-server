e48cc72173e5abdf83df238eab2fddeee0bd85ef
cardosofiles <cardosofiles@outlook.com>
2025-06-12 20:27:38 -0300

    chore: adding spring validation

--- src/main/java/br/com/cardosofiles/vacancy_management_server/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/cardosofiles/vacancy_management_server/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.cardosofiles.vacancy_management_server.modules.candidate;
 
 import java.util.UUID;
+import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -8,9 +11,16 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s8$).+", message = "Username inválido.")
     private String userName;
+
+    @Email(message = "E-mail inválido.")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
+
     private String description;
     private String curriculum;
 
--- src/main/java/br/com/cardosofiles/vacancy_management_server/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/cardosofiles/vacancy_management_server/modules/candidate/controllers/CandidateController.java
@@ -1,8 +1,11 @@
 package br.com.cardosofiles.vacancy_management_server.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.cardosofiles.vacancy_management_server.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -11,9 +14,13 @@ public class CandidateController {
 
 
     @PostMapping("/")
-    public void create() {
-
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getName());
+        System.out.println(candidateEntity.getUserName());
+        System.out.println(candidateEntity.getEmail());
+        System.out.println(candidateEntity.getPassword());
+        System.out.println(candidateEntity.getDescription());
     }
-
 
 }

c9eed82c85c526a1570379ef35937c104bff04d9
cardosofiles <cardosofiles@outlook.com>
2025-06-14 13:00:40 -0300

    chore: relationship between tables

---
+++ src/main/java/br/com/cardosofiles/vacancy_management_server/modules/company/entities/JobEntity.java
@@ -0,0 +1,37 @@
+package br.com.cardosofiles.vacancy_management_server.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+    private String benefits;
+    private String level;
+
+    @Column(name = "company_id", updatable = false)
+    @ManyToOne()
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdAt;
+}
